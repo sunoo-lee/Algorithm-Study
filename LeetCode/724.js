183c3be8ec8be53a667ad0cf8c237c0b0180b1aa
Sunoo <98771163+sunoo-lee@users.noreply.github.com>
2023-03-29 23:19:16 +0900

    Update and rename test.js to 724.js
    Find Pivot Index

---
+++ LeetCode/724.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let targetIndex = -1;
+    let leftSum = 0;
+    let rightSum = 0;
+
+    nums.forEach(item => leftSum = leftSum + item);
+ 
+    for ( let i=0; i<nums.length; ++i) {
+        let current = nums[i];
+
+        leftSum = leftSum - current;
+
+        if ( leftSum === rightSum ) {
+            targetIndex = i;
+            break;
+        }
+        rightSum = rightSum + current;
+    }
+    return targetIndex;
+};

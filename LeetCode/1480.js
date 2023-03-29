4d9e8945ba8a9a086d2f7d9452448b20dc3a4c83
Sunoo <98771163+sunoo-lee@users.noreply.github.com>
2023-03-29 23:18:19 +0900

    Create 1480.js
    Running Sum of 1d Array

---
+++ LeetCode/1480.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum = 0;
+    const sumNums = nums.map( (item) => { 
+        let a = item + sum;
+        sum = a;
+        return a;
+        } )
+    
+    return sumNums;
+};
